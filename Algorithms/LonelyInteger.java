f24962b39f4177c22c90c01c1b27a592c2ad1268
Arthur Lisboa <94870259+arthurlsb@users.noreply.github.com>
2022-08-15 02:05:18 -0300

    Create LonelyInteger.java

---
+++ Algorithms/LonelyInteger.java
@@ -0,0 +1,67 @@
+// The file name is not necessarily the same as the class name written in the code
+// https://www.hackerrank.com/challenges/lonely-integer
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<Integer> a) {
+        int x = 0;
+        int numberChoosed = 150;
+        
+        for(Integer number : a) {
+            numberChoosed = number;
+            
+            for(Integer y : a) {
+                if(y == numberChoosed) {
+                    x++;
+                }
+            }
+            if(x == 1) {
+                break;
+            }
+            x=0;
+        }
+        
+        return numberChoosed;
+            
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.lonelyinteger(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
