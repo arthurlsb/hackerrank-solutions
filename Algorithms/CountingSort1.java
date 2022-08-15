b7db16d84aae6194f3c92dcb1036da8120cb7ff4
Arthur Lisboa <94870259+arthurlsb@users.noreply.github.com>
2022-08-15 16:09:01 -0300

    Create CountingSort1.java

---
+++ Algorithms/CountingSort1.java
@@ -0,0 +1,64 @@
+// The file name is not necessarily the same as the class name written in the code
+// https://www.hackerrank.com/challenges/countingsort1
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'countingSort' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        int maxVal = Collections.max(arr, null);
+        List<Integer> frequencyList = new ArrayList<>();
+        
+        for(int i = 0; i <= maxVal; i++) {
+            frequencyList.add(0);
+        }
+        
+        for(Integer number : arr) {
+            int actualValue = frequencyList.get(number) + 1;
+            frequencyList.set(number, actualValue);
+        }
+        
+        return frequencyList;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
